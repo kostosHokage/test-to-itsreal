f826d674df68912574259f7b8a15f95613bb4eda
Konstantin <k.putivtsev@wizart.pro>
2022-12-25 14:35:58 +0300

    test

--- src/tasks/second-task/Element.jsx
+++ src/tasks/second-task/Element.jsx
@@ -1,19 +1,41 @@
-import React, { useRef } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import PropTypes from "prop-types";
 
-
 export default function Element(props) {
-    const ref = useRef(props.timer);
+    const {handlerElem, elements, id, index} = props
+
+    let max = 7;
+    let min = 3;
+    let random = Math.floor(Math.random() * (max - min) + min);
+
+    const intervalRef = useRef();
+    const [count, setCount] = useState(random)
 
     Element.propTypes = {
-        timer: PropTypes.number.isRequired,
-        elemId: PropTypes.number.isRequired
+        handlerElem: PropTypes.func.isRequired,
+        elements: PropTypes.array.isRequired,
+        id: PropTypes.string.isRequired,
+        index: PropTypes.number.isRequired
     };
 
+    useEffect(() => {
+        intervalRef.current = setInterval(() => {
+            console.log(elements)
+            setCount(count => count - 1)
+            if (count <= 1) {
+                handlerElem(elements.filter(i => i.id !== id))
+            }
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalRef.current)
+        }
+    }, [count])
+
     return (
-        <div className="square" key={props.elemId}>
-            <p className="elem-id">{props.elemId}</p>
-            <p className="elem-time">{ref.current}</p>
+        <div className="square" key={id}>
+            <p className="elem-id">{index}</p>
+            <p className="elem-time">{count}</p>
         </div>
     )
 }
--- src/tasks/second-task/SecondTask.jsx
+++ src/tasks/second-task/SecondTask.jsx
@@ -1,51 +1,21 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useState } from "react";
 import {Button} from 'antd';
 import Element from "./Element";
 
 export default function SecondTask() {
     const [elem, setElem] = useState([])
-    const ref = useRef(0);
-
-    function setTimer() {
-        let max = 5;
-        let min = 1;
-        let random = Math.floor(Math.random() * (max - min) + min);
-
-        setInterval(() => {
-            if (random > 0) {
-                random.toString();
-                return random--;
-            } else {
-                clearInterval()
-                return null;
-            }
-        }, 1000)
-    }
-
-    useEffect(() => {
-        for (let i = 0; i < elem.length; i++) {
-            setTimeout(function () {
-                if(ref.current >= 0) {
-                    setElem(elem.slice(elem[i], 1))
-                }
-            }, elem[i].time * 1000)
-        }
-    }, [elem])
 
     return (
         <div className="container">
             <Button type="primary" onClick={() => {
-                setElem([...elem, {time: setTimer()}])
-                console.log(elem)
+                setElem([...elem, {id: crypto.randomUUID()}])
             }}>
                 Add element
             </Button>
             {elem.map((el, index) => {
-                if(ref.current > 0) {
-                    return (
-                        <Element  elemId={index} timer={elem.time}/>
-                    )
-                } else return null;
+                return (
+                    <Element key={el.id} index={index} id={el.id} elements={elem} handlerElem={setElem}/>
+                )
             })}
         </div>
     );
